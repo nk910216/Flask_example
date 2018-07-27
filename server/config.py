eb7c9bbdad06365c69af1155512d3f82bcfd4581
nk910216 <nk910216@gmail.com>
2018-07-27 22:22:54 +0800

    add secret key into config

--- server/config.py
+++ server/config.py
@@ -4,6 +4,7 @@ class Config(object):
     """ Read the env setting from the env file into the config object
     """
     DB_URI = os.getenv('DB_URI', None)
+    SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 
 class RouteConfig(object):
