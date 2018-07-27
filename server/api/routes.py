113a1444834a4dcd9890d5783065be18449b19f7
nk910216 <nk910216@gmail.com>
2018-07-27 22:04:26 +0800

    add api route for user register

--- server/api/routes.py
+++ server/api/routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
 from flask_restplus import Api
 
+from server import auth
+
 api_bp = Blueprint('version_0', __name__)
 api = Api(api_bp)
 
+# auth
+api.add_resource(auth.RegisterHandler, '/user/register')
