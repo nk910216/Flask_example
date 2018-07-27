6a61db3dd446257a685fb08932370f1096b42f7c
nk910216 <nk910216@gmail.com>
2018-07-28 02:28:19 +0800

    add routes for posting picture

--- server/api/routes.py
+++ server/api/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restplus import Api
 
-from server import auth
+from server import auth, picture
 
 api_bp = Blueprint('version_0', __name__)
 api = Api(api_bp)
@@ -9,3 +9,6 @@ api = Api(api_bp)
 # auth
 api.add_resource(auth.RegisterHandler, '/user/register')
 api.add_resource(auth.LoginHandler, '/user/login')
+
+# picture
+api.add_resource(picture.PicturePostHandler, '/picture')
